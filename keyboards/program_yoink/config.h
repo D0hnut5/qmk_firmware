5d022ed0ea65098f1f8727165a91b3bac1cd0aa9
D0hnut5 <wmmillar@gmail.com>
2024-03-19 23:24:10 -0500

    Add ProgramYoink, first commit

--- keyboards/program_yoink/config.h
+++ keyboards/program_yoink/config.h
@@ -17,13 +17,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
+
+#define TAPPING_TERM 200
+#define TAPPING_TERM_PER_KEY
